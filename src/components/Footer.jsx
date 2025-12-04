8a5dbc7f06b3e0bad5b6bbe4a6fc0b4bd0a0ed5c
leocoelho1982 <craveirocoelho@gmail.com>
2025-12-04 22:13:08 +0000

    outras informações

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -67,7 +67,7 @@ export default function Footer() {
             <ul className="space-y-2">
               <li><a href="/eventos">:: Eventos</a></li>
               <li><a href="/atividades">:: Atividades</a></li>
-              <li><a href="/page/infouteis">:: Informações úteis</a></li>
+              <li><a href="/page/infouteis">:: Outras informações</a></li>
             </ul>
           </div>
 
